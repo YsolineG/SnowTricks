4af6329b3fc9187be7f2ee5ec60da5cccc65b836
YsolineG <ysoline.ganster@gmail.com>
2021-11-15 15:17:27 +0100

    feat: fetch all figures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,8 +16,13 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $figures = $this->getDoctrine()
+            ->getRepository(Figure::class)
+            ->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'figures' => $figures
         ]);
     }
 }
