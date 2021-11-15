23194afc9c5e01ca8041338f5de2f102ea01d0f3
YsolineG <ysoline.ganster@gmail.com>
2021-11-15 18:06:33 +0100

    feat: update a figure

---
+++ src/Controller/UpdateFigureController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Figure;
+use App\Form\FigureFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UpdateFigureController extends AbstractController
+{
+    #[Route('/update/figure/{id}', name: 'update_figure')]
+    public function index(Request $request, Figure $figure): Response
+    {
+        $form = $this->createForm(FigureFormType::class, $figure);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+        }
+
+        return $this->render('update_figure/index.html.twig', [
+            'figureForm' => $form->createView(),
+        ]);
+    }
+}
