8d91f9c7b5a3a2295c163427d52620b71ad8ff40
YsolineG <ysoline.ganster@gmail.com>
2021-11-15 12:21:20 +0100

    feat: add the figure form

--- src/Controller/AddFigureController.php
+++ src/Controller/AddFigureController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
+use App\Form\FigureFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AddFigureController extends AbstractController
 {
     #[Route('/add/figure', name: 'add_figure')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $figure = new Figure();
+        $form = $this->createForm(FigureFormType::class, $figure);
+
         return $this->render('add_figure/index.html.twig', [
-            'controller_name' => 'AddFigureController',
+            'figureForm' => $form->createView(),
         ]);
     }
 }
