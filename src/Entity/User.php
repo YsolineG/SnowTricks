e375196af5798c7c6097f83d85b289bd07cecf43
YsolineG <ysoline.ganster@gmail.com>
2022-02-07 19:26:12 +0100

    feat: add message unique entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Il existe déjà un compte avec cet email")
+ * @UniqueEntity(fields={"username"}, message="Il existe déjà un compte avec ce nom d'utilisateur")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
