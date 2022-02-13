176e6341c73b2b020cf37dfbd2f7fcf9dbc3b3b0
YsolineG <ysoline.ganster@gmail.com>
2022-02-13 18:11:54 +0100

    refactor: delete methods

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,27 +5,23 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
-use App\Security\EmailVerifier;
 use Swift_Mailer;
 use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-    private EmailVerifier $emailVerifier;
-
-    public function __construct(EmailVerifier $emailVerifier)
-    {
-        $this->emailVerifier = $emailVerifier;
-    }
-
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, Swift_Mailer $mailer): Response
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasherInterface,
+        Swift_Mailer $mailer
+    ): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -61,7 +57,7 @@ class RegistrationController extends AbstractController
             // On crée le message
             $message = (new Swift_Message('Activation de votre compte'))
                 // On attribue l'expéditeur
-                ->setFrom('no-reply@swon-tricks.com')
+                ->setFrom('no-reply@snow-tricks.com')
                 // On attribue le destinataire
                 ->setTo($user->getEmail())
                 // Oncrée le contenu
@@ -73,16 +69,6 @@ class RegistrationController extends AbstractController
             // On envoie l'email
             $mailer->send($message);
 
-            // generate a signed url and email it to the user
-//            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-//                (new TemplatedEmail())
-//                    ->from(new Address('ysoline.ganster@gmail.com', 'SnowTricks Mail Bot'))
-//                    ->to($user->getEmail())
-//                    ->subject('Please Confirm your Email')
-//                    ->htmlTemplate('registration/confirmation_email.html.twig')
-//            );
-            // do anything else you need here, like send an email
-
             return $this->redirectToRoute('home');
         }
 
@@ -91,28 +77,8 @@ class RegistrationController extends AbstractController
         ]);
     }
 
-    #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request): Response
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        // validate email confirmation link, sets User::isVerified=true and persists
-        try {
-            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $exception->getReason());
-
-            return $this->redirectToRoute('app_register');
-        }
-
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
-
-        return $this->redirectToRoute('app_register');
-    }
-
     #[Route('/activation/{token}', name: 'activation')]
-    public function activation($token, UserRepository $userRepository, Request $request)
+    public function activation($token, UserRepository $userRepository, Request $request): RedirectResponse
     {
         // On vérifie si un utilisateur a ce token
         $user = $userRepository->findOneBy(['activiation_token' => $token]);
@@ -128,7 +94,6 @@ class RegistrationController extends AbstractController
         $entityManager->flush();
 
         // On envoie un message flash
-//        $this->addFlash('success', 'Votre compte est activé');
         $request->getSession()->getFlashBag()->add('success', 'Votre compte est activé');
         return $this->redirectToRoute('home');
     }
